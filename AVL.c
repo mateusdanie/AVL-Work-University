f7bca913230847357a74973d00ebb73239ed5132
Mateus Daniel de Lima da Silva <mateusdaniel@alu.ufc.br>
2021-12-19 14:12:54 -0300

    Criado a função que verifica se a ABB é uma AVL

--- AVL.c
+++ AVL.c
@@ -7,3 +7,62 @@ int main()
 
     return 0;
 }
+
+int height(No *pointer){
+    int he, hd;
+
+    if(pointer->esq != NULL){
+       he = height(pointer->esq);
+    }else{
+        he = 0;
+    }
+        
+    if(pointer->dir != NULL){
+        hd = height(pointer->dir);
+    }else{
+        hd = 0;
+    }
+    
+    if(he > hd){
+        pointer->height = he + 1;
+    }else{
+        pointer->height = hd + 1;
+    }
+}
+
+int checkAVL(No *pointer, int maxNodes, int *count){
+    if(pointer->esq != NULL){
+        checkAVL(pointer->esq, maxNodes, count);
+    }
+
+    count++;
+
+    if(pointer->dir != NULL){
+        checkAVL(pointer->dir, maxNodes, count);
+    }
+
+    int he = height(pointer->esq);
+    int hd = height(pointer->dir);
+
+    if(hd - he > 1 || hd - he < -1){
+        printf("Nao eh AVL!!!\n");
+        return -1;
+    }else{
+        if(count == maxNodes){
+            printf("Eh AVL!!!\n");
+            return 0;
+        }
+    }
+}
+
+int countNodes(No *pointer, int *count){
+    if(pointer->esq != NULL){
+        countNodes(pointer->esq, count);
+    }
+
+    count++;
+
+    if(pointer->dir != NULL){
+        countNodes(pointer->dir, count);
+    }
+}
--- tree.h
+++ tree.h
@@ -2,12 +2,15 @@
 #define TREE_H
 
 typedef struct no{
-    int value;
+    int value, height;
     struct no *esq;
     struct no *dir;
 }No;
 
 int height(No *pointer);
+int checkAVL(No *pointer, int maxNodes, int *count);
+int countNodes(No *pointer, int *count);
+
 
 
 #endif
