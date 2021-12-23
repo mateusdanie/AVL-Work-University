c4bcffdf057c1bc09111c7c4f6213f0994ff4457
Mateus Daniel de Lima da Silva <mateusdaniel@alu.ufc.br>
2021-12-23 16:08:56 -0300

    Arrumado a função de adicionar e modificado o nome das rotações do adicionargit add -A!

--- AVL.c
+++ AVL.c
@@ -4,55 +4,65 @@
 
 int main()
 {
+    No *pointer = NULL;
+    int h, maxNodes = 0, count = 0;
 
+    
     return 0;
+}
+
+int preOrdem(No *pointer){
+
+    printf("No %d com bal %d\n", pointer->key, pointer->bal);
+
+    if(pointer->esq != NULL){
+        preOrdem(pointer->esq);
+    }
+
+    if(pointer->dir != NULL){
+        preOrdem(pointer->dir);
+    }
 }
 
 int height(No *pointer){
     int he, hd;
 
-    if(pointer->esq != NULL){
-       he = height(pointer->esq);
+    if(pointer == NULL){
+        return 0;
     }else{
-        he = 0;
-    }
-        
-    if(pointer->dir != NULL){
+        he = height(pointer->esq);
         hd = height(pointer->dir);
+    }
+
+    if(he > hd){
+        return he + 1;
     }else{
-        hd = 0;
+        return hd + 1;
     }
     
-    if(he > hd){
-        pointer->height = he + 1;
-    }else{
-        pointer->height = hd + 1;
-    }
 }
 
-int checkAVL(No *pointer, int maxNodes, int *count){
+int checkAVL(No *pointer, int maxNodes){
+    int count = 1;
+
     if(pointer->esq != NULL){
-        checkAVL(pointer->esq, maxNodes, count);
+       count = checkAVL(pointer->esq, maxNodes);
     }
 
-    count++;
-
     if(pointer->dir != NULL){
-        checkAVL(pointer->dir, maxNodes, count);
+        count = checkAVL(pointer->dir, maxNodes);
     }
 
     int he = height(pointer->esq);
     int hd = height(pointer->dir);
 
     if(hd - he > 1 || hd - he < -1){
-        printf("Nao eh AVL!!!\n");
-        return -1;
+        count = 0;
     }else{
-        if(count == maxNodes){
-            printf("Eh AVL!!!\n");
-            return 0;
-        }
+        count = 1;
     }
+
+    return count;
 }
 
 int countNodes(No *pointer, int *count){
@@ -60,7 +70,7 @@ int countNodes(No *pointer, int *count){
         countNodes(pointer->esq, count);
     }
 
-    count++;
+    (*count)++;
 
     if(pointer->dir != NULL){
         countNodes(pointer->dir, count);
@@ -86,67 +96,97 @@ int allBals(No *pointer){
     bal(pointer);
 }
 
-int rotation1(No *pointer, int *h){
-    No *pointerU = pointer->esq;
+int rotationADD1(No **pointer, int *h){
+    No *pointerU = (*pointer)->esq;
     bal(pointerU);
-    if(pointer->bal == 01){
-        pointer->esq = pointerU->dir;
-        pointerU->dir = pointer;
-        pointer = pointerU;
-        pointer->dir->bal = 0;
+    if((*pointer)-->bal == -1){
+        (*pointer)->dir = pointerU->dir;
+        pointerU->dir = (*pointer);
+        (*pointer) = pointerU;
+        (*pointer)->dir->bal = 0;
     }else{
         No *pointerV = pointerU->dir;
         bal(pointerV);
         pointerU->dir = pointerV->esq;
-        pointerV->esq = pointerV->dir;
-        pointerV->dir = pointer;
+        pointerV->esq = pointerU;
+        (*pointer)->esq = pointerV->dir;
+        pointerV->dir = (*pointer);
 
         if(pointerV->bal == 1){
-            pointer->bal = 0;
+            (*pointer)->bal = 0;
             pointerU->bal = -1;
         }else{
-            pointer->bal = 1;
+            (*pointer)->bal = 1;
             pointerU->bal = 0;
         }
-        pointer = pointerV;
+        (*pointer) = pointerV;
     }
 
-    pointer->bal = 0, h = 0;
+    (*pointer)->bal = 0, h = 0;
 }
 
-int rotation2(No *pointer, int *h){
+int rotationADD2(No **pointer, int *h){
+    No *pointerU = (*pointer)->dir;
+    bal(pointerU);
+    if((*pointer)->bal == -1){
+        (*pointer)->esq = pointerU->dir;
+        pointerU->dir = (*pointer);
+        (*pointer) = pointerU;
+        (*pointer)->dir->bal = 0;
+    }else{
+        No *pointerV = pointerU->dir;
+        bal(pointerV);
+        pointerU->dir = pointerV->esq;
+        pointerV->esq = pointerU;
+        pointerV->esq = pointerV->dir;
+        pointerV->dir = (*pointer);
 
+        if(pointerV->bal == 1){
+            (*pointer)->bal = -1;
+            pointerU->bal = 0;
+        }else{
+            (*pointer)->bal = 0;
+            pointerU->bal = 1;
+        }
+
+        (*pointer) = pointerV;
+    }
+
+    (*pointer)->bal = 0, h = 0;
 }
 
-int insertAVL(int x, No *pointer, int *h){
-    if(pointer == NULL){
-        pointer->key = x;
-        pointer->height = height(pointer);
-        pointer->bal = bal(pointer);
+int insertAVL(int x, No **pointer, int *h){
+    if((*pointer) == NULL){
+        No *node = (No*)malloc(sizeof(No));
+        node->esq = NULL;
+        node->dir = NULL;
+        node->bal = 0;
+        node->key = x;
+        (*pointer) = node;
         h = -1;
     }else{
-        if(x == pointer->key){
+        if(x == (*pointer)->key){
             printf("Elemento, encontrado!\n");
             return 1;
         }
 
-        if(x < pointer->key){
-            insertAVL(x, pointer->esq, h);
+        if(x < (*pointer)->key){
+            insertAVL(x,&((*pointer)->esq), h);
 
             if(h == -1){
-                switch (pointer->bal)
+                switch ((*pointer)->bal)
                 {
                 case 1:
-                    pointer->bal = 0;
+                    (*pointer)->bal = 0;
                     h = 0;
                     break;
                 
                 case 0:
-                    pointer->bal = 1;
+                   (*pointer)->bal = 1;
                     break;
 
                 case -1:
-                    rotation1(pointer, h);
+                    rotationADD1(pointer, h);
                     break;
 
                 default:
@@ -154,21 +194,21 @@ int insertAVL(int x, No *pointer, int *h){
                 }
             }
         }else{
-            insertAVL(x, pointer->dir, h);
+            insertAVL(x, &((*pointer)->dir), h);
             if(h == -1){
-                switch (pointer->bal)
+                switch ((*pointer)->bal)
                 {
                 case -1:
-                    pointer->bal = 0;
+                    (*pointer)->bal = 0;
                     h = 0;
                     break;
                 
                 case 0:
-                    pointer->bal = 1;
+                    (*pointer)->bal = 1;
                     break;
 
                 case 1:
-                    rotation2(pointer, h);
+                    rotationADD2(pointer, h);
                     break;
                     
                 default:
--- tree.h
+++ tree.h
@@ -2,19 +2,20 @@
 #define TREE_H
 
 typedef struct no{
-    int key, height, bal;
+    int key, bal;
     struct no *esq;
     struct no *dir;
 }No;
 
+int preOrdem(No *pointer);
 int height(No *pointer);
 int bal(No *pointer);
 int allBals(No *pointer);
-int rotation1(No *pointer, int *h);
-int rotation2(No *pointer, int *h);
-int checkAVL(No *pointer, int maxNodes, int *count);
+int rotationADD1(No **pointer, int *h);
+int rotationADD2(No **pointer, int *h);
+int checkAVL(No *pointer, int maxNodes);
 int countNodes(No *pointer, int *count);
-int insertAVL(int x, No *pointer, int *h);
+int insertAVL(int x, No **pointer, int *h);
 
 
 
